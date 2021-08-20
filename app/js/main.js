75ce123e3e9e3cd208c891635a3fb1104361d5ce
AlenaElena <41231908+AlenaElena@users.noreply.github.com>
2021-08-20 23:16:04 +0300

    added header, added section intro, section products-trend

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,28 @@
 $(function(){
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
 
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active')
 
-  
+  })
 
 })
+new Swiper('.intro-slider', {
+  loop: true,
+  loopFillGroupWithBlank: true,
+  speed: 3000,
+  pagination: {
+    el: '.intro-slider__pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    }
+  },
+
+});
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -42,26 +42,23 @@ function nunjucks() {
     .pipe(browserSync.stream())
   }
 
-
-
-  //со свайпером swiper
-function scripts() {
-  return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-
-    'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-    'node_modules/swiper/swiper-bundle.js',
-    'app/js/main.js',
-
-  ])
+  function scripts() {
+    return src([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      'node_modules/swiper/swiper-bundle.js',
+      'app/js/main.js'
+    ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
     .pipe(browserSync.stream())
-}
+  }
+
  
   function images() {
     return src('app/images/**/*.*')
